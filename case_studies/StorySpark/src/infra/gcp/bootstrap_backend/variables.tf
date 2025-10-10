3aed0dd6a150c80fdf3de67c09f47739bc5281c4
AndyMKC <amcheng@umich.edu>
2025-10-10 15:33:01 -0700

    Trying from gemini

---
+++ case_studies/StorySpark/src/infra/gcp/bootstrap_backend/variables.tf
@@ -0,0 +1,23 @@
+#
+# Terraform Variables for the Bootstrap Backend
+#
+
+# The Google Cloud Project ID (e.g., storyspark-555555)
+variable "project_id" {
+  description = "The ID of the GCP project where resources will be created."
+  type        = string
+}
+
+# The desired region for the GCS bucket (e.g., us-west1)
+variable "region" {
+  description = "The region for the GCS backend bucket."
+  type        = string
+}
+
+# The name of the GCS bucket to store the Terraform state (e.g., storyspark-555555-terraform-state)
+variable "bucket_name" {
+  description = "The globally unique name for the GCS bucket used for Terraform state storage."
+  type        = string
+}
+
+# Note: The zone variable is not used for GCS buckets, so we omit it here.
