64164145e8acd4cc0edc2f9759556ef2c4dcab6f
AndyMKC <amcheng@umich.edu>
2025-12-16 15:07:42 -0800

    explicit use of auth

--- case_studies/StorySpark/src/infra/gcp/main/versions.tf
+++ case_studies/StorySpark/src/infra/gcp/main/versions.tf
@@ -31,9 +31,11 @@ terraform {
 provider "google" {
   project = var.project_id
   region  = var.region
+  credentials = file(env.GOOGLE_APPLICATION_CREDENTIALS)
 }
 
 provider "google-beta" {
   project = var.project_id
   region  = var.region
+  credentials = file(env.GOOGLE_APPLICATION_CREDENTIALS)
 }
