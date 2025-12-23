583199a46006510adc0e2c62d7d0f303bed9f291
AndyMKC <amcheng@umich.edu>
2025-12-22 23:45:53 -0800

    adding the ability to mark a book as read

--- case_studies/StorySpark/src/app/books/get_recommendation.py
+++ case_studies/StorySpark/src/app/books/get_recommendation.py
@@ -8,19 +8,20 @@ router = APIRouter()
 @router.get("/books/recommendation", response_model=Book, operation_id="GetBookRecommendation")
 async def get_recommendation():
     # TODO:  Return books from database
-    from datetime import datetime, timezone
-    utc_now = datetime.now(timezone.utc)
-    book = Book(
-        id="internal_id",
-        title="req.title",
-        author="req.author",
-        owner_id="req.owner_id",
-        book_id="req.book_id",
-        relevant_text="req.relevant_text",
-        created_at=utc_now,
-        updated_at=utc_now,
-    )
-    return book
+    # from datetime import datetime, timezone
+    # utc_now = datetime.now(timezone.utc)
+    # book = Book(
+    #     id="internal_id",
+    #     title="req.title",
+    #     author="req.author",
+    #     owner_id="req.owner_id",
+    #     book_id="req.book_id",
+    #     relevant_text="req.relevant_text",
+    #     created_at=utc_now,
+    #     updated_at=utc_now,
+    # )
+    # return book
+    return None
     # if not _BOOK_STORE:
     #     raise HTTPException(status_code=404, detail="No books available")
     # return random.choice(list(_BOOK_STORE.values()))
