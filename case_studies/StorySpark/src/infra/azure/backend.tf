f3a15c5df1378a31ea463d8cf585b91a026dd6e1
AndyMKC <145952301+AndyMKC@users.noreply.github.com>
2026-01-01 22:33:29 -0800

    cleaning up unused stuff (#186)

--- case_studies/StorySpark/src/infra/azure/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "${var.bootstrap_backend_resource_group}"
-    storage_account_name = "${var.bootstrap_backend_storage_account}"
-    container_name       = "${var.bootstrap_backend_container}"
-    key                  = "${var.bootstrap_backend_key}"
-  }
-}
