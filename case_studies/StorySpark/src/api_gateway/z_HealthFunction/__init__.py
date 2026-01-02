3d03f3ae454d5c2183858cffe055256b424915dc
AndyMKC <amcheng@umich.edu>
2026-01-01 22:33:00 -0800

    cleaning up unused stuff

--- case_studies/StorySpark/src/api_gateway/z_HealthFunction/__init__.py
+++
@@ -1,5 +0,0 @@
-# https://storysparkapigateway.azurewebsites.net/api/health
-import azure.functions as func
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("SeeDawBelly", status_code=200)
--- case_studies/StorySpark/src/services/interfaces.py
+++
@@ -1,6 +0,0 @@
-from typing import Protocol, Dict
-
-class BookMetadataProvider(Protocol):
-    def fetch(self, all_isbns: list) -> Dict:
-        """Fetch normalized book metadata for a given ISBN."""
-        ...
